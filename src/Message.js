3ca82812c1b1dcbbc7984b608df78480810d9072
Sarthak Jain <sarthakjain7@outlook.com>
2021-06-14 18:51:05 +0530

    Modified messages by adding with senders name

--- src/Message.js
+++ src/Message.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Message(props) {
     return (
         <div>
-            <h2>{props.text}</h2>
+            <h2>{props.username}: {props.text}</h2>
         </div>
     )
 }
