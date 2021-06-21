e1215343d4d738b2523339a7eaae2c5c58488bf3
Sarthak Jain <sarthakjain7@outlook.com>
2021-06-21 15:17:17 +0530

    Added firebase file to connect with the database in real time

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+const firebaseApp=firebase.initializeApp({
+        apiKey: "AIzaSyAdD8AxOYoYDBXS9nOavUKu3OZ4QGGmbPY",
+        authDomain: "facebook-messenger-1d419.firebaseapp.com",
+        projectId: "facebook-messenger-1d419",
+        storageBucket: "facebook-messenger-1d419.appspot.com",
+        messagingSenderId: "419836976583",
+        appId: "1:419836976583:web:e6ea0be56a51d318248de2",
+        measurementId: "G-HMMR814HK7"
+      
+})
+
+const db = firebaseApp.firestore()
+
+export {db};
