0b27f023d644045e03ab5348f8c5a5e52f3365b8
Sarthak Jain <sarthakjain7@outlook.com>
2021-06-23 15:02:13 +0530

    Removed username for the user and added unknown for without name users

--- src/Message.js
+++ src/Message.js
@@ -1,26 +1,26 @@
 // rfce es7 snippet
 import React, { forwardRef } from 'react';
-import {Card, CardContent, Typography } from "@material-ui/core";
+import { Card, CardContent, Typography } from "@material-ui/core";
 import './Message.css'
 
-const Message = forwardRef(({message, username}, ref) => {
+const Message = forwardRef(({ message, username }, ref) => {
     const isUser = username === message.username; // checking if the person who is messaging is owner
     return (
         <div ref={ref} className={`message ${isUser && 'message_user'}`}>
-            <Card className = {isUser ? "message_usercard" : "message_guestcard"}>
-            {/* if the person isUser then a special class will be added message_user */}
+            <Card className={isUser ? "message_usercard" : "message_guestcard"}>
+                {/* if the person isUser then a special class will be added message_user */}
                 <CardContent>
                     <Typography
                         color="white"
                         variant="h5"
                         component="h2"
                     >
-                            {message.username}: {message.message}
+                        {!isUser && `${message.username || 'Unknown User'}: `} {message.message}
                     </Typography>
                 </CardContent>
             </Card>
-        </div>    
-                )
+        </div>
+    )
 })
 
 export default Message
